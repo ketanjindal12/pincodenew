fe2e36ad100962a11c3c60785bdd02496b92bcd5
ketan <ketanjindal000@gmail.com>
2023-03-25 23:52:43 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function PostalInfo() {
+  const [pincode, setPincode] = useState("");
+  const [postalInfo, setPostalInfo] = useState(null);
+
+  const handlePincodeChange = (event) => {
+    setPincode(event.target.value);
+  }
+
+  const handleGetInfo = async () => {
+    const url = `https://api.postalpincode.in/pincode/${pincode}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setPostalInfo(data[0].PostOffice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Enter Pincode</h2>
+      <div id='input'>
+      <input type="text" value={pincode} onChange={handlePincodeChange} placeholder="Pincode" />
+      </div>
+     
+      <button onClick={handleGetInfo}>Lookup</button>
+      {postalInfo && (
+        
+        <ul>
+          
+          {postalInfo.map((office) => (
+            <li key={office.Pincode}>
+              <div id='boarder'>
+              <p><strong>Name:</strong> {office.Name}</p>
+              <p><strong>Delivery Status:</strong> {office.DeliveryStatus}</p>
+              <p><strong>Contact:</strong> {office.Telephone}</p>
+              <p><strong>Address:</strong> {office.Description}</p>
+              </div>
+            </li>
+           
+          ))}
+        </ul>
+        
+      )}
     </div>
   );
 }
 
-export default App;
+export default PostalInfo;
